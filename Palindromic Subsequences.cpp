8dc0882e90ae7675b67ccef84f041c5647fe63f1
Ashraful Alam Ridoy <ashrafulalam1022002@gmail.com>
2025-01-27 00:57:48 +0600

    Create Palindromic Subsequences.cpp
    This repository contains C++ solutions for various Codeforces  problems

---
+++ Palindromic Subsequences.cpp
@@ -0,0 +1,42 @@
+
+
+#include <bits/stdc++.h>
+#include <cstdint>
+#define int long long
+#define pb push_back
+using namespace std;
+void solve(){
+    int n; cin >> n;
+    if(n==6){
+        cout<<1<<" "<<1<<" "<<2<<" "<<3<<" "<<1<<" "<<2<<endl;
+    }
+    else if(n==7){
+        cout<<1<<" "<<1<<" "<<2<<" "<<3<<" "<<1<<" "<<2<<" "<<3<<endl;
+    }
+    else{
+        if(n%2==0){
+            for(int i=1; i<=n/2; i++){
+                cout<<i<<" ";
+            }
+             for(int i=1; i<=n/2; i++){
+                cout<<i<<" ";
+            }
+            cout<<endl;
+        }
+        else{
+            for(int i=1;i<=n/2 ; i++){
+                cout<<i<<" ";
+            }
+            for(int i=1;i<=n/2 ; i++){
+                cout<<i<<" ";
+            }
+            cout<<(n/2)+1<<endl;
+        }
+    }
+}
+int32_t main() {
+	int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
